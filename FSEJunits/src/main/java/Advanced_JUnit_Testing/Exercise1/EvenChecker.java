c46fa276018366a0a4841a94f66ef6a00cec4f46
naniRam <yvenkatasriramreddy@gmail.com>
2026-07-05 18:40:05 +0530

    Added exercise 1 from adv junit

---
+++ FSEJunits/src/main/java/Advanced_JUnit_Testing/Exercise1/EvenChecker.java
@@ -0,0 +1,11 @@
+package Advanced_JUnit_Testing.Exercise1;
+
+
+
+public class EvenChecker {
+
+    public boolean isEven(int num)
+    {
+        return num % 2 == 0;
+    }
+}
---
+++ FSEJunits/src/test/java/Advanced_JUnit_Testing/Exercise1/EvenCheckerTest.java
@@ -0,0 +1,19 @@
+package Advanced_JUnit_Testing.Exercise1;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class EvenCheckerTest {
+
+    protected final EvenChecker e = new EvenChecker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2,4,6,9})
+    void testisEven(int num)
+    {
+        boolean res = e.isEven(num);
+        assertTrue(res);
+    }
+}
